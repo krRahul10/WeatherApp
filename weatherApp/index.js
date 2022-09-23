3ecf7241e80c79c0d4276b4ad7103d407d06a242
Rahul <rahul10kr7@gmail.com>
2022-09-23 20:29:21 +0530

    app done

--- weatherApp/index.js
+++ weatherApp/index.js
@@ -1,4 +1,4 @@
-let api = `https://api.openweathermap.org/data/2.5/weather?q=pune&appid=a8e163bc0b34c412cb3cb3aff29558b2`;
+// let api = `https://api.openweathermap.org/data/2.5/weather?q=pune&appid=a8e163bc0b34c412cb3cb3aff29558b2`;
 
 const http = require("http");
 
@@ -9,26 +9,36 @@ const requests = require("requests");
 const homeFile = fs.readFileSync("index.html", "utf-8");
 
 const replaceVal = (tempVal, orgVal) => {
-
-}
+  let temperature = tempVal.replace("{%tempVal%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("%{tempstatus}%", orgVal.weather[0].main);
+  return temperature;
+};
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    requests(`https://api.openweathermap.org/data/2.5/weather?q=pune&appid=a8e163bc0b34c412cb3cb3aff29558b2`,)
-      .on("data", (chunk) =>{
-        let objData = JSON.parse(chunk)
-        let arrData = [objData]
+    requests(
+      `https://api.openweathermap.org/data/2.5/weather?q=pune&appid=a8e163bc0b34c412cb3cb3aff29558b2`
+    )
+      .on("data", (chunk) => {
+        let objData = JSON.parse(chunk);
+        let arrData = [objData];
         // console.log(arrData[0].main.temp);
-        const realTimeData = arrData.map((val) => {
-            replaceVal(homeFile,val);
-        })
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+        // console.log(realTimeData)
       })
-      .on("end", (err) =>{
+      .on("end", (err) => {
         if (err) return console.log("connection closed due to errors", err);
-
-        console.log("end");
+        res.end();
+        // console.log("end");
       });
   }
 });
 
-server.listen(8000)
+server.listen(8000);
